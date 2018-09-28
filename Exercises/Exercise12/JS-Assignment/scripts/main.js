379cf2b2635e05c5fa7af9076b5e02ca8809d1aa
npjordan <42592456+npjordan@users.noreply.github.com>
2018-09-28 15:17:30 -0500

    Update main.js

--- Exercises/Exercise12/JS-Assignment/scripts/main.js
+++ Exercises/Exercise12/JS-Assignment/scripts/main.js
@@ -4,59 +4,55 @@ function update() {
     =====================*/
 	
     //Create new array.
-var messages = [];
+    var messages = [];
     //If first name is empty. Add it to the array
-if firstName.length =< 0{messages.push(First Name is required)
-	};
+    if (firstName.length <= 0){messages.push("First Name is required")};
+
     //If last name is empty. Add it to the array
-if lastName.length =< 0{messages.push(Last Name is required)
-	};
+    if (lastName.length <= 0){messages.push("Last Name is required")};
+
     //Email is empty. Add it to the array
-if email.length =< 0{messages.push(Email address is required)
-	};
+    if (email.length <= 0){messages.push("Email address is required")};
+
     //Loop through array
-var htmlString = "error-list";
-for(var i=0; i<messages.length; ++i){
-htmlString = htmlString + '<li>' + messages[i] + '</li>'
-    htmlString = htmlString + '</ul>';
-	console.log(error-list);
-};
+    var htmlString = "error-list";
+    for(var i=0; i<messages.length; ++i){
+    htmlString = htmlString + '<li>' + messages[i] + '</li>'
+        htmlString = htmlString + '</ul>';
+        console.log(error-list);
+    };
+
 
     //Create javascript object for the user
-var user = {
-	firstName = "Nicholas"
-	lastName = "Jordan"
-	email = "nicholas.jordan.10@gmail.com"
-	fullName: function(){
-var me = this;
-return me.firstName + " " + me.lastName;
-	}
-};
-console.log(user);
-
-var now = new Date();
-var new Date = new Date(2018, 9, 25);
+    var user = {
+        firstName : document.getElementById('firstName'),
+        lastName : document.getElementById("LastName"),
+        email : document.getElementById("emailAddress"),
+        fullName: function(){
+    return this.firstName + " " + this.lastName;
+        }
+    };
 
 
 
     //log the user object to the console
-
+    console.log(user);
 
     //Check to see if everything is valid
 
     //Create string for today's date. Remember, if you hard code this, it will be invalid tomorrow.
-
+    var now = new Date();
+    var newDate = new Date(2018, 9, 25);
     //Make the alert box.
-
-alert("Submitted on 'new Date'");
+    alert("Submitted on 'newDate'");
 
 
     /* ===================
         Your code ends here
     =====================*/
+    }
 
 
-}
 
 
 
