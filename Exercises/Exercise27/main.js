32f64cad95b423e028fbb15c4e99ced4c9b86ae2
npjordan <42592456+npjordan@users.noreply.github.com>
2018-10-03 14:09:51 -0500

    Update main.js

--- Exercises/Exercise27/main.js
+++ Exercises/Exercise27/main.js
@@ -1,26 +1,52 @@
 /*
     Exercise 27 
 */
-$(document).ready(function(){
-    /*=======================
-        Regular Expressions 
-     =======================*/
 
+
+
+
+
+$(document).ready(function(){
+/*=======================
+Regular Expressions 
+=======================*/
+var str = $('#email').val();
+var patt = new RegExp(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/);
+var res = patt.test(str);
+
+
+if (!res){
+    document.getElementById("email").classList.add(".is-invalid")
+};
+
+console.log(res);
 
 
 $('#btnvalidate').click(function(){
-    /*=======================
-       RegExp functionality 
-     =======================*/
+/*=======================
+  RegExp functionality 
+=======================*/
+var numeric = $('#numeric').val();
+var pattern = /[a-z]/gi;
+var numericResult = numeric.replace(pattern, '');
+console.log(numericResult);
 });
 
 
 
-     /*=======================
-       Array Methods 
-     =======================*/
+/*=======================
+  Array Methods 
+=======================*/
 
+const numbers = [1, 2, 3, 4
+];
 
+const squaredNumbers = numbers.map(
 
+function(element, index, array){
+    return Math.pow(element, 2);
+}
+
+);
 
 });
