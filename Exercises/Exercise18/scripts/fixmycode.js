eee51fdba8f134a9fd40800ed16c89e44db1d49a
npjordan <42592456+npjordan@users.noreply.github.com>
2018-10-18 14:44:16 -0500

    Update fixmycode.js

--- Exercises/Exercise18/scripts/fixmycode.js
+++ Exercises/Exercise18/scripts/fixmycode.js
@@ -1,34 +1,35 @@
 $(document).ready(function () {
 
 
-    $('#btnAdNumbers').on('click', function () {
+    $('#btnAddNumbers').on('click', function () {
         /*
             Retrieve the numbers from the input fields
         */
-        var firstNumber = $('txtFirstNumber').val();
-        var secondNumber = $('txtSecondNumber').val();
+		
+        var firstNumber = $('#txtFirstNumber').val();
+        var secondNumber = $('#txtSecondNumber').val();
         
         /*
             Add the two numbers together
         */
-        var sum = firstNumber + secondNumber;
+        var sum = parseInt(firstNumber) + parseInt(secondNumber);
 
         /*
             If our sum is greater than 10. Add a blue class to the input.
             If our sum is less than 10, add a red class
         */
         if(sum > 10){
-            $('txtThirdNumber').addClass('.blue');
-            $('txtThirdNumber').RemoveClass('red');
+            $('#txtThirdNumber').addClass('blue');
+            $('#txtThirdNumber').removeClass('red');
         }else{
-            $('txtThirdNumber').addClass('red');
-            $('txtThirdNumber').RemoveClass('.blue');
+            $('#txtThirdNumber').addClass('red');
+            $('#txtThirdNumber').removeClass('blue');
         }
 
         /*
             Update the third input field with our value.
         */
-        $('#txtThirNumber').val() = sum;
+        $('#txtThirdNumber').val(sum);
     });
 
 
